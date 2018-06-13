7a2b9881e0c8552dbb273485b41a343e14503296
G. Baecker <admin@o34.mooo.com>
2018-06-13 12:47:47 +0200

    Conceptual design implementation of Iconiq sale logic (for discussion)

--- contracts/VreoTokenSale.sol
+++ contracts/VreoTokenSale.sol
@@ -22,7 +22,7 @@ contract VreoTokenSale is PostKYCCrowdsale, TokenCappedCrowdsale, FinalizableCro
     uint public constant TOKEN_SHARE_OF_BOUNTY   =  50000000e18;  // =  50.000.000 e18
 
     // Extra token percentages
-    uint public constant BONUS_PCT_IN_ICONIQ_SALE       = 20;  // TBD
+    uint public constant BONUS_PCT_IN_ICONIQ_SALE       = 30;  // TBD
     uint public constant BONUS_PCT_IN_VREO_SALE_PHASE_1 = 20;
     uint public constant BONUS_PCT_IN_VREO_SALE_PHASE_2 = 10;
 
@@ -128,12 +128,68 @@ contract VreoTokenSale is PostKYCCrowdsale, TokenCappedCrowdsale, FinalizableCro
         emit RateChanged(_newRate);
     }
 
+    function getMaximumPossibleInvestment(address _investor) public returns (uint) {
+        // No tokens available anymore?
+        if (remainingTokens == 0) {
+            return 0;
+        }
+
+        // Iconiq sale period
+        if (ICONIQ_SALE_OPENING_TIME <= now && now <= ICONIQ_SALE_CLOSING_TIME) {
+            // How many MEROs the investor purchased so far.
+            uint balance;
+            if (investments[_investor].isVerified) {
+                balance = investments[_investor].tokenAmount;
+            }
+            else {
+                balance = token.balanceOf(_investor);
+            }
+
+            // Prorata limit
+            uint iconiqBalance = iconiq.balanceOf(_investor);
+            uint prorataLimit = TOTAL_TOKEN_CAP_OF_SALE.mul(iconiqBalance).div(iconiqTotalEstimate);
+
+            // Did the investor already reached his prorata limit?
+            if (balance >= prorataLimit) {
+                return 0;
+            }
+
+            // How many additional MEROs the investor can buy.
+            uint tokenLimit = prorataLimit - balance;
+            if (tokenLimit > remainingTokens) {
+                tokenLimit = remainingTokens;
+            }
+
+            return tokenLimit.div(rate.mul((100 + BONUS_PCT_IN_ICONIQ_SALE) / 100));
+        }
+
+        // Vreo sale period
+        if (VREO_SALE_OPENING_TIME <= now && now <= VREO_SALE_CLOSING_TIME) {
+            if (now <= VREO_SALE_PHASE_1_END_TIME) {
+                return remainingTokens.div(rate.mul((100 + BONUS_PCT_IN_VREO_SALE_PHASE_1) / 100));
+            }
+
+            if (now <= VREO_SALE_PHASE_2_END_TIME) {
+                return remainingTokens.div(rate.mul((100 + BONUS_PCT_IN_VREO_SALE_PHASE_2) / 100));
+            }
+
+            return remainingTokens.div(rate);
+        }
+
+        // Outside of any sale period
+        return 0;
+    }
+
     /// @dev Pre validate purchase
     /// @param _beneficiary An Ethereum address
     /// @param _weiAmount A positive number
     function _preValidatePurchase(address _beneficiary, uint _weiAmount) internal {
-        require(ICONIQ_SALE_OPENING_TIME <= now && now <= ICONIQ_SALE_CLOSING_TIME && iconiq.isAllowed(msg.sender)
-                || VREO_SALE_OPENING_TIME <= now && now <= VREO_SALE_CLOSING_TIME);
+        //require(ICONIQ_SALE_OPENING_TIME <= now && now <= ICONIQ_SALE_CLOSING_TIME && iconiq.isAllowed(msg.sender)
+        //        || VREO_SALE_OPENING_TIME <= now && now <= VREO_SALE_CLOSING_TIME);
+
+        uint maximumPossibleInvestment = getMaximumPossibleInvestment(msg.sender);
+
+        require(0 < maximumPossibleInvestent && _weiAmount <= maximumPossibleInvestment);
 
         super._preValidatePurchase(_beneficiary, _weiAmount);
     }
--- docs/diagrams/tools/README.rst
+++ docs/diagrams/tools/README.rst
@@ -13,11 +13,13 @@ Classes:
 
 - won't be written if part of a package
 - depending on the stereotype will become
+
   - contracts (if there's no stereotype)
   - interfaces (if stereotype is `«interface»`)
   - libraries (if stereotype is `«library»`)
   - structs (if stereotype is `«struct»`)
   - enums (if stereotype is `«enum»`)
+
 - structs and enums will be written into the contracts/interfaces/libraries
   when properly connected, regardless if this is valid code
 
@@ -25,22 +27,28 @@ Attributes:
 
 - are written as state variables regardless if their occurrence is valid code
 - visibility will become
+
   - public (if visibility is `public`/`+`)
   - internal (if visibility is `protected`/`#`)
   - private (if visibility is `private`/`–`)
+
 - cannot be set to constant
 
 Operations:
 
 - depending on the stereotype or name will become
+
   - constructors (if name is `constructor` or equals class name)
   - modifiers (if stereotype is `«modifier»`)
   - events (if stereotype is `«event»`)
+
 - visibility will become
+
   - external (if part of a library)
   - public (if visibility is `public`/`+`)
   - internal (if visibility is `protected`/`#`)
   - private (if visibility is `private`/`–`)
+
 - will be set to `view` if they're queries (`constant`)
 - modifiers and super-constructors will be recognized from comment lines,
   regardless if they are not in inheritance chain
@@ -49,4 +57,3 @@ Requirements:
 
 - `gunzip` (likely part of `gzip`)
 - `xsltproc` (maybe part of `libxml2`)
-
