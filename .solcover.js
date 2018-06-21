9fe8795077a1aef4034d83f06a5706b842abe493
Your Name <you@example.com>
2018-06-21 18:49:07 +0200

    removed solidity coverage, since it didn´t work because it does not support snapshots

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,7 @@
 module.exports = {
 
     compileCommand: 'truffle compile',
+    port: 8545,
     norpc: true,
     testCommand: 'truffle test --network coverage',
     copyPackages: ['zeppelin-solidity'],
