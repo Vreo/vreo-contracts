56ae3ff76870ca95d58d7205ebf4aed2917cca3e
Your Name <you@example.com>
2018-06-21 15:44:12 +0200

    solcover

---
+++ .solcover.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    compileCommand: 'truffle compile',
+    norpc: true,
+    testCommand: 'truffle test --network coverage',
+    copyPackages: ['zeppelin-solidity'],
+};
+
+
+
--- truffle.js
+++ truffle.js
@@ -6,6 +6,14 @@ module.exports = {
             network_id: "*",
             host: "127.0.0.1",
             port: 8545
+        },
+    
+    	coverage: {
+      	    host: "localhost",
+            network_id: "*",
+            port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+            gas: 0xfffffffffff, // <-- Use this high gas value
+            gasPrice: 0x01      // <-- Use this low gas price
         }
     }
 };
